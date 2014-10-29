e5e30b923902a51da225e1fc8bd6ab3717ec62f3
iskutar <i.skutar@svitla.com>
2014-10-29 13:35:52 +0200

    comments to db

---
+++ db/comments_clubs.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2014 at 05:50 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `clubs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clubs`
+--
+
+CREATE TABLE IF NOT EXISTS `clubs` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) COLLATE utf8_bin NOT NULL, -- кодування utf8_general_ci
+  `city` varchar(20) COLLATE utf8_bin NOT NULL, -- city_id і звязок із іншою таблицею
+  `country` varchar(20) COLLATE utf8_bin NOT NULL, -- має бути country_id в таблиці cities
+  `league` varchar(35) COLLATE utf8_bin NOT NULL, -- league_id, типу int
+  `year` int(4) NOT NULL,
+  `prizes` int(5) NOT NULL,
+  `budget` int(15) NOT NULL,
+  `president` varchar(20) COLLATE utf8_bin NOT NULL, --president_id і звязок із таблицею presidents
+  `stadium` varchar(20) COLLATE utf8_bin NOT NULL, -- stadium_id, int
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leagues`
+--
+
+CREATE TABLE IF NOT EXISTS `leagues` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) COLLATE utf8_bin NOT NULL,
+  `country` varchar(20) COLLATE utf8_bin NOT NULL,
+  `rate` float NOT NULL,
+  `president` varchar(30) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prizes`
+--
+
+CREATE TABLE IF NOT EXISTS `prizes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) COLLATE utf8_bin NOT NULL,
+  `year` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stadiums`
+--
+
+CREATE TABLE IF NOT EXISTS `stadiums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) COLLATE utf8_bin NOT NULL,
+  `year` int(4) NOT NULL,
+  `roominess` int(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
